534912259117f661577dff8183fb367f2a336229
Christopher Herrera <cth0910@gmail.com>
2023-03-24 13:13:42 -0700

    Add icons to navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import React, { FC, useState } from "react";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
-import X from "./X";
+import X from "@/components/X";
+import HamburgerMenu from "@/components/HamburgerMenu";
 
 export default function NavBar() {
   const { data: session, status } = useSession();
@@ -23,19 +24,7 @@ export default function NavBar() {
             onClick={() => setOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-6 w-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <HamburgerMenu />
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="mt-4 flex flex-col p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 ">
