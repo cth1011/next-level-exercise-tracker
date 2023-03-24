35f20627c8b0eef241b11b3140f0b6cc774a1683
Christopher Herrera <cth0910@gmail.com>
2023-03-24 14:31:43 -0700

    Fix mobile login button

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -10,15 +10,15 @@ export default function NavBar() {
   return (
     <>
       <nav className="px-6 py-6 sm:px-4">
-        <div className="container mx-auto flex flex-wrap items-center justify-between">
-          <span className="self-center whitespace-nowrap text-xl font-extrabold text-rose-700">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <span className="self-center text-xl font-extrabold whitespace-nowrap text-rose-700">
             NextLevel
           </span>
 
           <button
             data-collapse-toggle="navbar-default"
             type="button"
-            className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
             aria-controls="navbar-default"
             aria-expanded="false"
             onClick={() => setOpen(true)}
@@ -27,11 +27,11 @@ export default function NavBar() {
             <HamburgerMenu />
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="mt-4 flex flex-col p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 ">
+            <ul className="flex flex-col p-4 mt-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 ">
               <li>
                 <Link
                   href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 >
                   Pricing
                 </Link>
@@ -39,7 +39,7 @@ export default function NavBar() {
               <li>
                 <Link
                   href="/api/auth/signin"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                   onClick={(e) => {
                     e.preventDefault();
                     signIn();
@@ -55,11 +55,11 @@ export default function NavBar() {
       {isOpen && (
         <div className="lg:hidden" role="dialog" aria-modal="true">
           <div className="fixed inset-0 z-10"></div>
-          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="">
                 <span className="sr-only">NextLevel</span>
-                <span className="self-center whitespace-nowrap text-xl font-extrabold text-rose-700">
+                <span className="self-center text-xl font-extrabold whitespace-nowrap text-rose-700">
                   NextLevel
                 </span>
               </a>
@@ -71,15 +71,19 @@ export default function NavBar() {
                 <X onClick={() => setOpen(false)} />
               </button>
             </div>
-            <div className="mt-6 flow-root">
+            <div className="flow-root mt-6">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="py-6">
-                  <a
-                    href="#"
+                  <Link
+                    href="/api/auth/signin"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signIn();
+                    }}
                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
