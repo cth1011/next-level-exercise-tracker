749efcd6ddb40e4e3f87baa76f9fcb25995465bf
Christopher Herrera <cth0910@gmail.com>
2023-03-24 13:12:10 -0700

    Add get started navigation

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC = () => (
 
         <div className="mt-8 flex flex-wrap gap-4 text-center">
           <Link
-            href="#"
+            href="/app"
             className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
           >
             Get Started
