a55a2ff4672cfcfc70f70320598ec0657f24bae6
Christopher Herrera <cth0910@gmail.com>
2023-03-25 16:35:00 -0700

    Add settings link

--- frontend/components/UserMenu.tsx
+++ frontend/components/UserMenu.tsx
@@ -14,24 +14,24 @@ const UserMenu = ({ open, setOpen }: IUserMenu) => {
     <div
       className={`absolute right-0 top-12 z-50 my-4 ${
         open ? "" : "hidden"
-      } list-none divide-y divide-gray-100 rounded bg-white text-base shadow `}
+      } min-w-[150px] list-none divide-y divide-gray-100 rounded bg-white text-base shadow `}
     >
       <div className="px-4 py-3" role="none">
         <p className="text-sm text-gray-900" role="none">
-          {session?.user?.name || "User Name"}
+          {session?.user?.name || "Guest"}
         </p>
         <p className="text-sm font-medium text-gray-900 truncate " role="none">
-          {session?.user?.email || "user@email.com"}
+          {session?.user?.email || ""}
         </p>
       </div>
       <ul className="py-1" role="none">
         <li>
           <Link
-            href="#"
+            href="/app/settings"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
             role="menuitem"
           >
-            Dashboard
+            Settings
           </Link>
         </li>
 
