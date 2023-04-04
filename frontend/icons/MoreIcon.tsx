25d8182da79d13132c056f1227be8375d17ccc0b
Christopher Herrera <cth0910@gmail.com>
2023-04-04 04:13:08 -0700

    Move icons folder

--- frontend/icons/MoreIcon.tsx
+++ frontend/icons/MoreIcon.tsx
@@ -1,11 +1,15 @@
-const MoreIcon = () => (
+interface IMoreIcon {
+  color?: string;
+}
+
+const MoreIcon = ({ color = "text-gray-900" }: IMoreIcon) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className={`h-6 w-6 ${color}`}
   >
     <path
       strokeLinecap="round"
