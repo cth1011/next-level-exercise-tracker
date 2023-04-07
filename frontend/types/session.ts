ad3756342b004e6e47ed50157cb6e40aa6b9d98d
Christopher Herrera <cth0910@gmail.com>
2023-04-07 12:44:34 -0700

    Add types

--- frontend/types/session.ts
+++ frontend/types/session.ts
@@ -1,22 +1,30 @@
-export type WorkoutSession = {};
-
-export type SetSession = {
-  workout_name: string;
-  exercise_name: string;
-  rep: number;
-  set: number;
-};
+import { EDIT, IN_PROGRESS, NOT_STARTED, VIEW } from "@/constants";
 
 export type Template = {
   template_id: number;
   workout_name: string;
   exercises: Exercise[];
-  last_date_performed: Date;
+  last_date_performed?: Date;
 };
 
 export type Exercise = {
-  set?: number;
-  reps?: number;
-  exercise_name: string;
+  name: string;
+  type: string;
+  sets?: Set[];
+  unit?: Unit;
+  muscle: string;
+  equipment: string;
+  difficulty: string;
+  instructions: string;
+};
+
+export type Set = {
+  set_no: number;
+  reps: number;
+  previous?: number;
   weight: number;
 };
+
+export type WorkoutStatus = typeof NOT_STARTED | typeof IN_PROGRESS;
+export type ExercisePageStatus = typeof VIEW | typeof EDIT;
+export type Unit = "lbs" | "kg";
