631cc6ec0db00f3d6f8ebed7e6555435e3efa2be
Christopher Herrera <cth0910@gmail.com>
2023-04-09 12:02:24 -0700

    Prevent propagation when clicking on Modal

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -9,7 +9,7 @@ interface IModal {
 }
 
 const Modal = ({ id, btnActionLabel, title, onClick, children }: IModal) => (
-  <>
+  <div onClick={(e) => e.stopPropagation()}>
     <input type="checkbox" id={id} className="modal-toggle" />
     <label htmlFor={id} className="cursor-pointer modal">
       <label className="relative max-w-xs modal-box" htmlFor="">
@@ -23,6 +23,7 @@ const Modal = ({ id, btnActionLabel, title, onClick, children }: IModal) => (
           <label
             htmlFor={id}
             onClick={onClick}
+            data-testid="modal-action-button"
             className="btn-ghost btn-sm btn text-rose-700"
           >
             {btnActionLabel || "OK"}
@@ -30,7 +31,7 @@ const Modal = ({ id, btnActionLabel, title, onClick, children }: IModal) => (
         </div>
       </label>
     </label>
-  </>
+  </div>
 );
 
 export default Modal;
