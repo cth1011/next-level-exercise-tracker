a0df09ad3e7f0270d7d06313a4ef22bfdde50232
Christopher Herrera <cth0910@gmail.com>
2023-04-16 15:23:09 -0700

    Fix attributes for icons

--- frontend/icons/X.tsx
+++ frontend/icons/X.tsx
@@ -4,17 +4,17 @@ interface IX {
 
 const X: React.FC<IX> = ({ onClick }) => (
   <svg
-    className="h-6 w-6"
+    className="w-6 h-6"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
     aria-hidden="true"
     onClick={onClick}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M6 18L18 6M6 6l12 12"
     />
   </svg>
