399fa14b1bc180b3f48f240d879ef185b96fc7dd
Christopher Herrera <cth0910@gmail.com>
2023-05-01 23:41:57 -0700

    Fix fetch bug

--- frontend/pages/app/workout/index.tsx
+++ frontend/pages/app/workout/index.tsx
@@ -7,25 +7,22 @@ import AddIcon from "@/icons/AddIcon";
 import { useRouter } from "next/router";
 import { useWorkoutStore } from "@/stores/useWorkoutStore";
 import React, { Suspense } from "react";
-import { GUEST_EMAIL, IN_PROGRESS } from "@/constants";
+import { GUEST_EMAIL, IN_PROGRESS, TEMPLATE_CREATION } from "@/constants";
 import Loading from "./loading";
 
-import { fetchTemplates } from "@/hooks/useTemplates";
-import { getServerSession } from "next-auth";
-import { GetServerSideProps } from "next";
+import { fetchTemplates } from "@/hooks/api";
+import { GetStaticProps } from "next";
 
 const Workout: React.FC = () => {
   const router = useRouter();
 
   const setWorkoutStatus = useWorkoutStore((state) => state.setWorkoutStatus);
-  const setWorkoutSession = useWorkoutStore((state) => state.setWorkoutSession);
 
   const { data: session } = useSession();
   const email = session?.user?.email || GUEST_EMAIL;
 
-  const { data: templates } = useQuery(
-    ["templates", email],
-    () => fetchTemplates(email)
+  const { data: templates } = useQuery(["templates", email], () =>
+    fetchTemplates(email)
   );
 
   return (
@@ -45,27 +42,29 @@ const Workout: React.FC = () => {
       <div className="mt-2">
         <div className="flex items-center justify-between">
           <span className="text-sm text-gray-500">Workout Templates</span>
-          <button type="button" className="btn-ghost btn-xs btn-circle btn">
+          <button
+            type="button"
+            className="btn-ghost btn-xs btn-circle btn"
+            onClick={() => {
+              setWorkoutStatus(TEMPLATE_CREATION);
+              router.push("/app/workout/start");
+            }}
+          >
             <AddIcon />
           </button>
         </div>
         <Suspense fallback={<Loading />}>
           <div
             data-testid="templates"
-            className="w-full mt-2 md: md:flex md:space-x-2"
+            className="w-full gap-2  md:grid md:grid-cols-2 md:gap-3 lg:grid-cols-3"
           >
             {templates?.map((template, index) => {
               return (
-                <div
-                  data-testid={`template-${index}`}
-                  key={index}
-                  className="md:w-1/2"
-                >
+                <div data-testid={`template-${index}`} key={index}>
                   <WorkoutTemplate
                     template={template}
                     onClick={() => {
                       setWorkoutStatus(IN_PROGRESS);
-                      setWorkoutSession(template);
                       router.push("/app/workout/start");
                     }}
                   />
@@ -79,11 +78,10 @@ const Workout: React.FC = () => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
+export const getStaticProps: GetStaticProps = async () => {
   const queryClient = new QueryClient();
-  const email = session?.user?.email || GUEST_EMAIL;
-  await queryClient.prefetchQuery(["templates", email], () =>
+  const email = GUEST_EMAIL;
+  await queryClient.fetchQuery(["templates", email], () =>
     fetchTemplates(email)
   );
   return {
