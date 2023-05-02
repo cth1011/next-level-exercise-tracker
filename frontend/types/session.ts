f136edd9cc647ef8e2426d2d163ca8a3f59203e0
Christopher Herrera <cth0910@gmail.com>
2023-05-01 23:44:45 -0700

    Modify types

--- frontend/types/session.ts
+++ frontend/types/session.ts
@@ -1,9 +1,9 @@
-import { EDIT, IN_PROGRESS, NOT_STARTED, VIEW } from "@/constants";
+import { EDIT, IN_PROGRESS, NOT_STARTED, VIEW, TEMPLATE_CREATION } from "@/constants";
 
 export type Template = {
   id: number;
   workout_name: string;
-  exercises: TemplateExercise[];
+  exercises?: TemplateExercise[];
   last_date_performed?: Date;
   user_id: number;
 };
@@ -18,21 +18,33 @@ export type TemplateExercise = {
 }
 
 export type Exercise = {
+  id?: number;
   name: string;
   type: string;
-  sets?: Set[];
+  sets?: WorkoutSet[];
   muscle: string;
   equipment: string;
   difficulty: string;
   instructions: string;
 };
 
-export type Set = {
+export type WorkoutSession = {
+  workout_name: string;
+}
+
+export type WorkoutExercise = {
+  exercise_id: number;
+  name: string;
+  sets: WorkoutSet[]
+}
+
+export type WorkoutSet = {
   set_no: number;
-  reps: number;
+  reps?: number;
   previous?: number;
-  weight: number;
+  weight?: number;
+  checked?: boolean;
 };
 
-export type WorkoutStatus = typeof NOT_STARTED | typeof IN_PROGRESS;
+export type WorkoutStatus = typeof NOT_STARTED | typeof IN_PROGRESS | typeof TEMPLATE_CREATION;
 export type ExercisePageStatus = typeof VIEW | typeof EDIT;
